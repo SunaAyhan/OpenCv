b647a32e0387423c0006396c5d19cfa7f2368556
SunaAyhan <sunaayhan16@gmail.com>
2021-04-24 23:30:08 +0300

    cv2

---
+++ 23_custom_cascade/cascade.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 24 20:53:48 2021
+
+@author: Suna Ayhan
+"""
+"""
+ 1) veri seti oluştur
+ 2) cascade programı indir
+ 3) cascade
+ 4) cascade kullanarak tespit algoritması yaz
+
+"""
+import cv2
+import os
+
+# resim depo klasörü
+
+path = "images"
+
+#resim boyutu
+imgWidth = 180
+imgHeight = 120
+
+#video capture
+cap = cv2.VideoCapture(0)  #default kamera old. için => 0
+cap.set(3,640), cap.set(4, 480) #kamera pixeli
+cap.set(10, 180) #aydınlık seviyesi
+
+global countFolder
+def saveDataFunc():
+    global countFolder
+    countFolder=0
+    while os.path.exists(path + str(countFolder)):
+        countFolder+=1
+    os.makedirs(path+str(countFolder))
+saveDataFunc()
+
+count = 0
+countSave = 0
+while True:
+    success, img = cap.read()
+    if success:
+        img = cv2.resize(img,(imgWidth, imgHeight))
+        if count % 5 ==0:
+            cv2.imwrite(path + str(countFolder)+"/"+str(countSave)+"_"+".png", img)
+            countSave += 1
+            print(countSave)
+            count += 1
+            cv2.imshow("image",img)
+        if cv2.waitKey(1) & 0xFF == ord("q"): break
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
